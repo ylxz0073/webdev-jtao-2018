659d93f1e14da29f099a50faaec58f1c973a64f9
ylxz0073 <jiapengtao@yahoo.com>
2018-05-23 04:59:54 -0700

    create register controller

---
+++ src/main/webapp/jquery/components/register/register.controller.client.js
@@ -0,0 +1,19 @@
+(function() {
+    var $usernameFld,
+        $passwordFld,
+        $verifyPasswordFld;
+    var $registerBtn;
+    var userService = new UserServiceClient();
+
+    $(main);
+
+    function main() {
+
+    }
+
+    function register() {
+
+    }
+
+    
+})
