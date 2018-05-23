e17d39b224cb8a14f8227e54fca1b3955de1a4a0
ylxz0073 <jiapengtao@yahoo.com>
2018-05-22 21:26:18 -0700

    add user JPA repository

---
+++ src/main/java/webdev/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package webdev.repositories;
+
+import org.apache.catalina.User;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+
+}
+
+
+
