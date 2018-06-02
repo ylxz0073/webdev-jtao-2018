deec67447693aabf51a0a71730f24c1eb0c0719e
ylxz0073 <jiapengtao@yahoo.com>
2018-06-02 16:55:07 -0700

    add course service post mapping

--- src/main/java/com/example/coursemanagementsystem2018/services/CourseServices.java
+++ src/main/java/com/example/coursemanagementsystem2018/services/CourseServices.java
@@ -1,13 +1,17 @@
 package com.example.coursemanagementsystem2018.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.coursemanagementsystem2018.models.Course;
 import com.example.coursemanagementsystem2018.repositories.CourseRepository;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class CourseServices {
 	@Autowired
 	CourseRepository courseRepository;	
@@ -15,5 +19,10 @@ public class CourseServices {
 	@GetMapping("/api/course")
 	public Iterable<Course> findAllCourses() {
 		return courseRepository.findAll(); 
+	}
+	
+	@PostMapping("/api/course")
+	public Course createCourse(@RequestBody Course course) {
+		return courseRepository.save(course);
 	}
 }
