4ada93791135eec5fb8aaddf79c838b3e56cb924
ylxz0073 <jiapengtao@yahoo.com>
2018-06-03 22:33:08 -0700

    add lesson delete mapping

--- src/main/java/com/example/coursemanagementsystem2018/services/LessonService.java
+++ src/main/java/com/example/coursemanagementsystem2018/services/LessonService.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.coursemanagementsystem2018.models.Course;
@@ -26,8 +29,23 @@ public class LessonService {
 	@Autowired
 	LessonRepository lessonRepository;
 	
+	@PostMapping("/api/course/{courseId}/module/{moduleId}/lesson")
+	public Lesson createLesson(@PathVariable("moduleId") int moduleId,
+			@RequestBody Lesson newLesson) {
+			
+			Optional<Module> data = moduleRepository.findById(moduleId);
+			if(data.isPresent()) {
+				Module module = data.get();
+				newLesson.setModule(module);
+				return lessonRepository.save(newLesson);
+			}
+			System.out.println("##### " + moduleId + " data not present #####");
+			return null;
+
+		}
+	
 	@GetMapping("/api/course/{courseId}/module/{moduleId}/lesson")
-	public List<Lesson> findAllLessonsForModule(@PathVariable("courseId") int courseId,
+	public List<Lesson> findAllLessonsForModule(
 			@PathVariable("moduleId") int moduleId){
 		Optional<Module> data = moduleRepository.findById(moduleId);
 		if(data.isPresent()) {
@@ -38,6 +56,13 @@ public class LessonService {
 		}
 		return null;
 		
+	}
+	
+	@DeleteMapping("/api/course/{courseId}/module/{moduleId}/lesson/{lessonId}")
+	public void deleteLesson(
+	  @PathVariable("lessonId")
+	    int lessonId) {
+		lessonRepository.deleteById(lessonId);
 	}
 	
 
